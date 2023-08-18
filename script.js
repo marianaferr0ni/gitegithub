73454c9f017ce703b1608f3c0087b106d2169b4d
mari <mariana.ferroni@estudante.ifms.edu.br>
2023-08-17 22:04:14 -0400

    update js

--- script.js
+++ script.js
@@ -30,3 +30,43 @@ truco1Button.addEventListener('click', () => {
     pontos + 3;
     updateCount();
 })
+
+function gerarCor()
+{
+  var r,g,b;
+  r = Math.floor(Math.random()*255);
+  g = Math.floor(Math.random()*255);
+  b = Math.floor(Math.random()*255);
+  return 'rgb('+r+','+g+','+b+')';
+}
+function mudarAqui(elemento)
+{
+  var novaCor = gerarCor();
+  elemento.style.backgroundColor = novaCor;
+}
+function mudarPar()
+{
+  var numeroPar = document.getElementsByClassName("par");
+  var novaCor = gerarCor();
+  for (let i of numeroPar)
+  {
+    i.style.backgroundColor = novaCor;
+  }
+}
+function mudarImpar()
+{
+  let numeroImpar = document.getElementsByClassName("impar");
+  var novaCor = gerarCor();
+  for (let i of numeroImpar)
+  {
+    i.style.backgroundColor = novaCor;
+  }
+}
+function apagarTudo()
+{
+  let apagar = document.getElementsByTagName("div");
+  for (let i of apagar)
+  {
+    i.style.backgroundColor = "white";
+  }
+}
